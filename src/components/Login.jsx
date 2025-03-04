4770e4fe42b98152dbe89946f9712dcaf62cba68
Rohitbanala <o180361@rguktong.ac.in>
2025-03-04 19:12:35 +0530

    lernt about different ways to get user input and validate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,73 @@
+import { useState } from "react";
+import Inputting from "./Input.jsx";
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+  function handleBlur(identifier) {
+    setDidEdit((prevValues) => {
+      return { ...prevValues, [identifier]: true };
+    });
+  }
+  const emailInValid = didEdit.email && !enteredValues.email.includes("@");
+  function handleInputChange(identifier, event) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: event.target.value,
+    }));
+    setDidEdit((prevValues) => {
+      return { ...prevValues, [identifier]: false };
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("login form submitted");
+    console.log("email:" + enteredValues.email);
+    console.log("password:" + enteredValues.password);
+    setEnteredValues({ email: "", password: "" });
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+  // function handleEnteredPassword(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
-        </div>
-
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
-        </div>
+        <Inputting
+          label="Email"
+          id="email"
+          name="email"
+          onBlur={() => handleBlur("email")}
+          onChange={(event) => handleInputChange("email", event)}
+          value={enteredValues.email}
+        />
+        <Inputting
+          label="Password"
+          id="password"
+          name="password"
+          onBlur={() => handleBlur("password")}
+          onChange={(event) => handleInputChange("password", event)}
+          value={enteredValues.password}
+        />
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" onClick={handleSubmit}>
+          Login
+        </button>
       </p>
     </form>
   );
